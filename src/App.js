6c5efdac1a7ed3a36ca2209976107726046a3e42
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-07-11 11:28:25 +0100

    Added link to github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
+      <footer>
+        <a href="https://github.com/v-victoria/dictionary-react">
+          Open-source code
+        </a>
+        , by Victoria Martynenko
+      </footer>
     </div>
   );
 }
